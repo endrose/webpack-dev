d4dab0d696d73be507fd3fa9938bf1b40ee1527c
Endrose <endros007@gmail.com>
2021-01-24 02:53:42 +0700

    config

---
+++ configs/env.js
@@ -0,0 +1,9 @@
+const path = require('path')
+const path = require("path")
+
+module.exports = {
+    paths: {
+        source: path.resolve(__dirname, "../src"),
+        output: path.resolve(__dirname, "../dist")
+    }
+};
